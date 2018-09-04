ddeb2e639e710fa118eb41a58fa04d1369ed6df2
Alex Pilin <hopgosy@gmail.com>
2018-09-04 14:10:18 +0400

    js DOM vers. 2.0

--- js/index.js
+++ js/index.js
@@ -1,5 +1,32 @@
-this.window.onload = function() {
-  window.document.getElementById("mainInfo").innerHTML = JSON.stringify(
-    weekForecastMock
-  );
-};
+//Дополнительное задание
+var diagram_array = document.getElementsByClassName('diagram');
+var buttonss_array = document.getElementsByClassName('buttonss');
+var mainInfoTable = document.getElementById('main-buttons');
+
+mainInfoTable.addEventListener('click', function(event){
+    if(event.target.classList.contains('buttonss')){
+      index = parseInt(event.target.dataset.btnindex);
+      showMainInfo(index);
+    }
+})
+
+function showMainInfo() {
+  for (var i = 0; i < diagram_array.length; i++) {
+    diagram_array[i].classList.remove('active1');
+  }
+  for (var i = 0; i < buttonss_array.length; i++) {
+    buttonss_array[i].classList.remove('active2');
+  }
+  if(index == 0){
+    diagram_array[0].classList.add('active1');
+    buttonss_array[0].classList.add('active2');
+  }
+  else if(index == 1){
+    diagram_array[1].classList.add('active1');
+    buttonss_array[1].classList.add('active2');
+  }
+  else if(index == 2){
+    diagram_array[2].classList.add('active1');
+    buttonss_array[2].classList.add('active2');
+  }
+}
--- js/script.js
+++ js/script.js
@@ -1,188 +1,78 @@
-var element = document.getElementsByClassName('activity');
-var time = document.getElementsByClassName('time-of-day-temperature');
-var illumination = document.getElementsByClassName('trigger-button')
-
-function nextclick() {
-
-    element[0].classList.add('block-none');
-    element[0].nextElementSibling.classList.add('activity');
-    element[0].nextElementSibling.classList.remove('block-none');
-    element[0].classList.remove('activity');
-    if (element[0].getAttribute('class') == 'button-next activity'){
-      element[0].classList.remove('activity');
-      time[0].classList.add('activity');
-      time[0].classList.remove('block-none');
-  }
-  if (time[0].getAttribute('class') == 'time-of-day-temperature activity') {
-    illumination[0].style.backgroundColor = '#86FF73';
-    illumination[4].style.backgroundColor = 'white';
-  }
-  if (time[1].getAttribute('class') == 'time-of-day-temperature activity') {
-    illumination[1].style.backgroundColor = '#86FF73';
-    illumination[0].style.backgroundColor = 'white';
-  }
-  if (time[2].getAttribute('class') == 'time-of-day-temperature activity') {
-    illumination[2].style.backgroundColor = '#86FF73';
-    illumination[1].style.backgroundColor = 'white';
-  }
-  if (time[3].getAttribute('class') == 'time-of-day-temperature activity') {
-    illumination[3].style.backgroundColor = '#86FF73';
-    illumination[2].style.backgroundColor = 'white';
-  }
-  if (time[4].getAttribute('class') == 'time-of-day-temperature activity') {
-    illumination[4].style.backgroundColor = '#86FF73';
-    illumination[3].style.backgroundColor = 'white';
-  }
-}
-function backclick() {
-  element[0].classList.add('block-none');
-  element[0].previousElementSibling.classList.add('activity');
-  element[0].nextElementSibling.classList.remove('activity');
-  element[0].classList.remove('block-none');
-  if (element[0].getAttribute('class') == 'button-back rotate-strelka activity'){
-    element[0].classList.remove('activity');
-    time[4].classList.add('activity');
-    time[4].classList.remove('block-none');
-  }
-  if (time[0].getAttribute('class') == 'time-of-day-temperature activity') {
-    illumination[0].style.backgroundColor = '#86FF73';
-    illumination[1].style.backgroundColor = 'white';
-  }
-  if (time[1].getAttribute('class') == 'time-of-day-temperature activity') {
-    illumination[1].style.backgroundColor = '#86FF73';
-    illumination[2].style.backgroundColor = 'white';
-  }
-  if (time[2].getAttribute('class') == 'time-of-day-temperature activity') {
-    illumination[2].style.backgroundColor = '#86FF73';
-    illumination[3].style.backgroundColor = 'white';
-  }
-  if (time[3].getAttribute('class') == 'time-of-day-temperature activity') {
-    illumination[3].style.backgroundColor = '#86FF73';
-    illumination[4].style.backgroundColor = 'white';
-  }
-  if (time[4].getAttribute('class') == 'time-of-day-temperature activity') {
-    illumination[4].style.backgroundColor = '#86FF73';
-    illumination[0].style.backgroundColor = 'white';
-  }
-}
-
-
-function trigger__button1(){
-  time[0].classList.add('activity');
-  time[0].classList.remove('block-none');
-  time[1].classList.remove('activity');
-  time[2].classList.remove('activity');
-  time[3].classList.remove('activity');
-  time[4].classList.remove('activity');
-  time[1].classList.add('block-none');
-  time[2].classList.add('block-none');
-  time[3].classList.add('block-none');
-  time[4].classList.add('block-none');
-  illumination[0].style.backgroundColor = '#86FF73';
-  illumination[1].style.backgroundColor = 'white';
-  illumination[2].style.backgroundColor = 'white';
-  illumination[3].style.backgroundColor = 'white';
-  illumination[4].style.backgroundColor = 'white';
-}
-
-function trigger__button2(){
-  time[0].classList.remove('activity');
-  time[0].classList.add('block-none');
-  time[1].classList.add('activity');
-  time[2].classList.remove('activity');
-  time[3].classList.remove('activity');
-  time[4].classList.remove('activity');
-  time[1].classList.remove('block-none');
-  time[2].classList.add('block-none');
-  time[3].classList.add('block-none');
-  time[4].classList.add('block-none');
-  illumination[0].style.backgroundColor = 'white';
-  illumination[1].style.backgroundColor = '#86FF73';
-  illumination[2].style.backgroundColor = 'white';
-  illumination[3].style.backgroundColor = 'white';
-  illumination[4].style.backgroundColor = 'white';
-}
-
-function trigger__button3(){
-  time[0].classList.remove('activity');
-  time[0].classList.add('block-none');
-  time[1].classList.remove('activity');
-  time[2].classList.add('activity');
-  time[3].classList.remove('activity');
-  time[4].classList.remove('activity');
-  time[1].classList.add('block-none');
-  time[2].classList.remove('block-none');
-  time[3].classList.add('block-none');
-  time[4].classList.add('block-none');
-  illumination[0].style.backgroundColor = 'white';
-  illumination[1].style.backgroundColor = 'white';
-  illumination[2].style.backgroundColor = '#86FF73';
-  illumination[3].style.backgroundColor = 'white';
-  illumination[4].style.backgroundColor = 'white';
-}
-
-function trigger__button4(){
-  time[0].classList.remove('activity');
-  time[0].classList.add('block-none');
-  time[1].classList.remove('activity');
-  time[2].classList.add('block-none');
-  time[3].classList.remove('block-none');
-  time[4].classList.remove('activity');
-  time[1].classList.add('block-none');
-  time[2].classList.remove('activity');
-  time[3].classList.add('activity');
-  time[4].classList.add('block-none');
-  illumination[0].style.backgroundColor = 'white';
-  illumination[1].style.backgroundColor = 'white';
-  illumination[2].style.backgroundColor = 'white';
-  illumination[3].style.backgroundColor = '#86FF73';
-  illumination[4].style.backgroundColor = 'white';
-}
-
-function trigger__button5(){
-  time[0].classList.remove('activity');
-  time[0].classList.add('block-none');
-  time[1].classList.remove('activity');
-  time[2].classList.add('block-none');
-  time[3].classList.add('block-none');
-  time[4].classList.add('activity');
-  time[1].classList.add('block-none');
-  time[2].classList.remove('activity');
-  time[3].classList.remove('activity');
-  time[4].classList.remove('block-none');
-  illumination[0].style.backgroundColor = 'white';
-  illumination[1].style.backgroundColor = 'white';
-  illumination[2].style.backgroundColor = 'white';
-  illumination[3].style.backgroundColor = 'white';
-  illumination[4].style.backgroundColor = '#86FF73';
-}
-
-
-//Дополнительное задание
+var elements = document.getElementsByClassName('time-of-day-temperature');
+var  wetherbetter = document.getElementById('weatherbox');
+var triggerity = document.getElementsByClassName('trigger-button');
 var diagram_array = document.getElementsByClassName('diagram');
 var buttonss_array = document.getElementsByClassName('buttonss');
+wetherbetter.addEventListener('click', function(event){
+    if(event.target.classList.contains('clicked-next')){
+      showNext();
+    }
+    else if(event.target.classList.contains('clicked-back')){
+      showPrev();
+    }
+    else if(event.target.classList.contains('trigger-button')){
+      index = parseInt(event.target.dataset.btnindex);
+      showSpecialDay(index);
+    }
+})
 
-function diagrammy__1(){
-diagram_array[0].classList.remove('diagram-none');
-diagram_array[1].classList.add('diagram-none');
-diagram_array[2].classList.add('diagram-none');
-buttonss_array[0].classList.add('active2');
-buttonss_array[1].classList.remove('active2');
-buttonss_array[2].classList.remove('active2');
+function showNext(){
+for (var i = 0; i < elements.length; i++) {
+      if (elements[i].classList.contains('activity')) {
+        elements[i].classList.remove('activity');
+        triggerity[i].classList.remove('activus');
+        if(i === elements.length-1) {
+          elements[0].classList.add('activity');
+          triggerity[0].classList.add('activus');
+      }
+        elements[i].nextElementSibling.classList.add('activity');
+        triggerity[i].nextElementSibling.classList.add('activus');
+        break;
+      }
+    }
 }
-function diagrammy__2(){
-diagram_array[0].classList.add('diagram-none');
-diagram_array[1].classList.remove('diagram-none');
-diagram_array[2].classList.add('diagram-none');
-buttonss_array[0].classList.remove('active2');
-buttonss_array[1].classList.add('active2');
-buttonss_array[2].classList.remove('active2');
+
+function showPrev(){
+  for (var i = 0; i < elements.length; i++) {
+          if (elements[i].classList.contains('activity')){
+            elements[i].classList.remove('activity');
+            triggerity[i].classList.remove('activus');
+            if(i === 0) {
+              elements[elements.length-1].classList.add('activity');
+              triggerity[elements.length-1].classList.add('activus');
+          }
+            elements[i].previousElementSibling.classList.add('activity');
+            triggerity[i].previousElementSibling.classList.add('activus');
+            break;
+          }
+        }
 }
-function diagrammy__3(){
-diagram_array[0].classList.add('diagram-none');
-diagram_array[1].classList.add('diagram-none');
-diagram_array[2].classList.remove('diagram-none');
-buttonss_array[0].classList.remove('active2');
-buttonss_array[1].classList.remove('active2');
-buttonss_array[2].classList.add('active2');
-}
+
+function showSpecialDay(){
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].classList.remove('activity');
+  }
+  for (var i = 0; i < triggerity.length; i++) {
+    triggerity[i].classList.remove('activus');
+  }
+  if(index == 0 ){
+    triggerity[0].classList.add('activus');
+    elements[0].classList.add('activity');
+  }
+  else if(index == 1){
+    triggerity[1].classList.add('activus');
+    elements[1].classList.add('activity');
+  }
+  else if(index == 2){
+    triggerity[2].classList.add('activus');
+    elements[2].classList.add('activity');
+  }
+  else if(index == 3){
+    triggerity[3].classList.add('activus');
+    elements[3].classList.add('activity');
+  }
+  else if(index == 4){
+    triggerity[4].classList.add('activus');
+    elements[4].classList.add('activity');
+  }
+  }
